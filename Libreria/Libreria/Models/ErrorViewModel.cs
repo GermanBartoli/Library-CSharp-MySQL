5eb8fcd42c8a5d659ea69a4f451bbfe96da70679
German Bartoli <bartoligerman497@gmail.com>
2022-07-21 17:54:16 -0300

    Proyecto Libreria  - Initial Commit

---
+++ Libreria/Libreria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Libreria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
