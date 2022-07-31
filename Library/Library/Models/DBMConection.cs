4b0bdf6a863d96f0967f778f3ab4fc3cd5bb5744
German Bartoli <bartoligerman497@gmail.com>
2022-07-31 16:51:43 -0300

    DBMConection

---
+++ Library/Library/Models/DBMConection.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Models;
+
+public static class DBMConection
+{
+    private static string conectionString = "";
+
+    public static string ConectionString { get => conectionString; set => conectionString = value; }
+}
--- Library/Library/Models/GestorBDConexion.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BitsionFicticiaSA.Models;
-
-public static class GestorBDConexion
-{
-    private static string conexionString = "";
-
-    public static string ConexionString { get => conexionString; set => conexionString = value; }
-}
