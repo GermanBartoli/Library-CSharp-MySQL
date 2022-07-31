7aa9cd66907a329f34839b29b39552e524f02203
German Bartoli <bartoligerman497@gmail.com>
2022-07-31 14:30:24 -0300

    Create MartialStatusModel.cs

---
+++ Library/Library/Models/Client/MartialStatus/MartialStatusModel.cs
@@ -0,0 +1,21 @@
+namespace Library.Models.Client;
+
+public class MartialStatusModel
+{
+    private int martial_status_id = 0;
+    private string description = "";
+
+    public MartialStatusModel(int martial_status_id, string description)
+    {
+        this.Martial_status_id = martial_status_id;
+        this.Description = description;
+    }
+
+    public MartialStatusModel()
+    {
+
+    }
+
+    public int Martial_status_id { get => martial_status_id; set => martial_status_id = value; }
+    public string Description { get => description; set => description = value; }
+}
