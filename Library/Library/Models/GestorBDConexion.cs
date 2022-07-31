77cefc5c98752cf48a83cfcef821f45c49867c60
German Bartoli <bartoligerman497@gmail.com>
2022-07-31 16:06:12 -0300

    *

--- Library/Library/Models/GestorBDConexion.cs
+++ Library/Library/Models/GestorBDConexion.cs
@@ -2,7 +2,7 @@
 
 public static class GestorBDConexion
 {
-    private static string conexionString;
+    private static string conexionString = "";
 
     public static string ConexionString { get => conexionString; set => conexionString = value; }
 }
