f5071d01b7f9a82aa32d1d29fafe23300fdb3cc7
German Bartoli <bartoligerman497@gmail.com>
2022-07-31 16:09:48 -0300

    Update ClientModel.cs

--- Library/Library/Models/Client/ClientModel.cs
+++ Library/Library/Models/Client/ClientModel.cs
@@ -18,4 +18,31 @@ public class ClientModel
     private DateTime update_at;
     private Boolean active = false;
 
+    public ClientModel(int client_id, MartialStatusModel martial_status, string name, string email, DateTime birthdate, GenderClient gender, DateTime create_at, DateTime update_at, bool active)
+    {
+        this.client_id = client_id;
+        this.martial_status = martial_status;
+        this.name = name;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.gender = gender;
+        this.create_at = create_at;
+        this.update_at = update_at;
+        this.active = active;
+    }
+
+    public ClientModel()
+    {
+
+    }
+
+    public int Client_id { get => client_id; set => client_id = value; }
+    public MartialStatusModel Martial_status { get => martial_status; set => martial_status = value; }
+    public string Name { get => name; set => name = value; }
+    public string Email { get => email; set => email = value; }
+    public DateTime Birthdate { get => birthdate; set => birthdate = value; }
+    public GenderClient Gender { get => gender; set => gender = value; }
+    public DateTime Create_at { get => create_at; set => create_at = value; }
+    public DateTime Update_at { get => update_at; set => update_at = value; }
+    public bool Active { get => active; set => active = value; }
 }
