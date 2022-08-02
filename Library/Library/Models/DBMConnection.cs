2e18f44f652586d725f748ce5c8d00092a835e1c
German Bartoli <bartoligerman497@gmail.com>
2022-08-01 22:07:35 -0300

    Create DBMConnection.cs

---
+++ Library/Library/Models/DBMConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Models;
+
+public static class DBMConnection
+{
+    private static string connectionString = "";
+
+    public static string ConnectionString { get => connectionString; set => connectionString = value; }
+}
