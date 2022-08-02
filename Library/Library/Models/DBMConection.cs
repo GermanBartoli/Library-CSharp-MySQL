fec56ae88a38652bbe35b99bcaa54a065a24e122
German Bartoli <bartoligerman497@gmail.com>
2022-08-01 22:07:33 -0300

    Delete DBMConection.cs

--- Library/Library/Models/DBMConection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Library.Models;
-
-public static class DBMConection
-{
-    private static string conectionString = "";
-
-    public static string ConectionString { get => conectionString; set => conectionString = value; }
-}
