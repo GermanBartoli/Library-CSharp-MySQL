af852c685cad7024d73de789e1604defa434de64
German Bartoli <bartoligerman497@gmail.com>
2022-08-11 23:26:39 -0300

    Update ClientController.cs

--- Library/Library/Controllers/ClientController.cs
+++ Library/Library/Controllers/ClientController.cs
@@ -22,7 +22,85 @@ namespace Library.Controllers
             return View(dto);
         }
 
+        [HttpPost]
+        public IActionResult ClientList(DTOModel dto)
+        {
+            dto.ClientList = new();
 
+            return View(dto);
+        }
+
+        public ActionResult NewClient(int? Client_Id)
+        {
+            List<MartialStatusModel> martialStatus = dBMMartialStatus.LoadComboBoxMartialStatus();
+
+            ViewBag.MartialStatus = martialStatus;
+
+            DTOModel dto = new();
+
+            if (Client_Id == 0 || Client_Id == null)
+            {
+                return View(dto);
+            }
+            else
+            {
+                dto = dBMClient.GetClientByClientID(Client_Id);
+                if (dto == null)
+                {
+                    return NotFound();
+                }
+                return View(dto);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult NewClient(DTOModel dto)
+        {
+            if (dto.Client.Client_Id == 0)
+            {
+                bool successAddClient;
+
+                successAddClient = dBMClient.AddClient(dto.Client);
+                dto.Client.Client_Id = dBMClient.GetLastClientByID();
+
+                if (successAddClient)
+                {
+                    return RedirectToAction("ConsultClient", "Client", new { Client_Id = dto.Client.Client_Id });
+                }
+                else
+                {
+                    return RedirectToAction("ClientList", "Client");
+                }
+            }
+            else
+            {
+                bool successEditClient;
+
+                successEditClient = dBMClient.EditClient(dto.Client);
+
+                if (successEditClient)
+                {
+                    return RedirectToAction("ConsultClient", "Client", new { Client_Id = dto.Client.Client_Id });
+                }
+                else
+                {
+                    return RedirectToAction("ClientList", "Client");
+                }
+            }
+        }
+
+        public ActionResult DisableClient(int Client_Id)
+        {
+            dBMClient.DisableClient(Client_Id);
+
+            return RedirectToAction("ClientList", "Client");
+        }
+
+        [HttpPost]
+        public IActionResult DisableClient(ClientModel client)
+        {
+            return View("ClientList");
+        }
 
         public ActionResult ConsultClient(int? Client_Id)
         {
@@ -40,7 +118,7 @@ namespace Library.Controllers
             {
                 DTOModel dto = new();
 
-                dto = dBMClient.GetClientByCustomerID(Client_Id);
+                dto = dBMClient.GetClientByClientID(Client_Id);
 
                 if (dto == null)
                 {
