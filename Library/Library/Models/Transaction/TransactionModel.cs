3c4cee84048368ae0daebd74cebb5ef78740c13c
German Bartoli <bartoligerman497@gmail.com>
2022-08-17 12:38:08 -0300

    Update TransactionModel.cs

--- Library/Library/Models/Transaction/TransactionModel.cs
+++ Library/Library/Models/Transaction/TransactionModel.cs
@@ -5,26 +5,24 @@ namespace Library.Models.Transaction;
 
 public class TransactionModel
 {
-    private int transaction_id = 0;
-    private BookModel book = new();
-    private ClientModel client = new();
+    private int transaction_id;
+    private BookModel book;
+    private ClientModel client;
     private DateTime date;
-
-    public enum EstateTransaction
-    { Lend, Sell, Return };
-
-    private EstateTransaction estate;
+    private String estate;
     private DateTime created_at;
     private DateTime updated_at;
-    private bool finished = false;
-    private bool active = false;
+    private bool finished;
+    private bool active;
+
+    //public enum EstateTransaction { Lend, Sell, Return };
 
     public TransactionModel(
         int transaction_id,
         BookModel book,
         ClientModel client,
         DateTime date,
-        EstateTransaction estate,
+        String estate,
         DateTime created_at,
         DateTime updated_at,
         bool finished,
@@ -43,15 +41,24 @@ public class TransactionModel
 
     public TransactionModel()
     {
+        transaction_id = 0;
+        book = new();
+        client = new();
+        date = DateTime.Now;
+        estate = "";
+        created_at = DateTime.Now;
+        updated_at = DateTime.Now;
+        finished = false;
+        active = false;
     }
 
     public int Transaction_id { get => transaction_id; set => transaction_id = value; }
     public BookModel Book { get => book; set => book = value; }
     public ClientModel Client { get => client; set => client = value; }
     public DateTime Date { get => date; set => date = value; }
+    public String Estate { get => estate; set => estate = value; }
     public DateTime Created_at { get => created_at; set => created_at = value; }
     public DateTime Updated_at { get => updated_at; set => updated_at = value; }
     public bool Finished { get => finished; set => finished = value; }
     public bool Active { get => active; set => active = value; }
-    private EstateTransaction Estate { get => estate; set => estate = value; }
 }
