fb8b34a11df849300e20403eaa3dee0af4b71551
German Bartoli <bartoligerman497@gmail.com>
2022-08-18 22:27:57 -0300

    Update DTOModel.cs

--- Library/Library/Models/DTO/DTOModel.cs
+++ Library/Library/Models/DTO/DTOModel.cs
@@ -1,4 +1,5 @@
-﻿using Library.Models.Client;
+﻿using Library.Models.Author;
+using Library.Models.Client;
 
 namespace Library.Models.DTO;
 
@@ -7,18 +8,31 @@ public class DTOModel
     private List<ClientModel> clientList;
     private ClientModel client;
 
-    public DTOModel(List<ClientModel> clientList, ClientModel client)
+    private List<AuthorModel> authorList;
+    private AuthorModel author;
+
+    public DTOModel(
+        List<ClientModel> clientList,
+        ClientModel client,
+        List<AuthorModel> authorList,
+        AuthorModel author)
     {
         this.clientList = clientList;
         this.client = client;
+        this.authorList = authorList;
+        this.author = author;
     }
 
     public DTOModel()
     {
         clientList = new();
         client = new();
+        authorList = new();
+        author = new();
     }
 
     public List<ClientModel> ClientList { get => clientList; set => clientList = value; }
     public ClientModel Client { get => client; set => client = value; }
+    public List<AuthorModel> AuthorList { get => authorList; set => authorList = value; }
+    public AuthorModel Author { get => author; set => author = value; }
 }
