106d23a7d83695f0d106a82faf9dc5d42371e516
German Bartoli <bartoligerman497@gmail.com>
2022-08-21 13:50:18 -0300

    Update DTOModel.cs

--- Library/Library/Models/DTO/DTOModel.cs
+++ Library/Library/Models/DTO/DTOModel.cs
@@ -1,38 +1,65 @@
 ﻿using Library.Models.Author;
+using Library.Models.Book;
 using Library.Models.Client;
+using Library.Models.Transaction;
 
 namespace Library.Models.DTO;
 
 public class DTOModel
 {
-    private List<ClientModel> clientList;
     private ClientModel client;
+    private List<ClientModel> clientList;
 
-    private List<AuthorModel> authorList;
     private AuthorModel author;
+    private List<AuthorModel> authorList;
 
-    public DTOModel(
-        List<ClientModel> clientList,
+    private BookModel book;
+    private List<BookModel> bookList;
+
+    private TransactionModel transaction;
+    private List<TransactionModel> transactionList;
+
+        public DTOModel(
         ClientModel client,
+        List<ClientModel> clientList,
+        AuthorModel author,
         List<AuthorModel> authorList,
-        AuthorModel author)
+        BookModel book,
+        List<BookModel> bookList,
+        TransactionModel transaction,
+        List<TransactionModel> transactionList)
     {
-        this.clientList = clientList;
         this.client = client;
-        this.authorList = authorList;
+        this.clientList = clientList;
         this.author = author;
+        this.authorList = authorList;
+        this.book = book;
+        this.bookList = bookList;
+        this.transaction = transaction;
+        this.transactionList = transactionList;
     }
 
     public DTOModel()
     {
-        clientList = new();
         client = new();
-        authorList = new();
+        clientList = new();
+
         author = new();
+        authorList = new();
+
+        book = new();
+        bookList = new();
+
+        transactionList = new();
+        transaction = new();
     }
 
-    public List<ClientModel> ClientList { get => clientList; set => clientList = value; }
     public ClientModel Client { get => client; set => client = value; }
-    public List<AuthorModel> AuthorList { get => authorList; set => authorList = value; }
+    public List<ClientModel> ClientList { get => clientList; set => clientList = value; }
     public AuthorModel Author { get => author; set => author = value; }
+    public List<AuthorModel> AuthorList { get => authorList; set => authorList = value; }
+    public BookModel Book { get => book; set => book = value; }
+    public List<BookModel> BookList { get => bookList; set => bookList = value; }
+    public TransactionModel Transaction { get => transaction; set => transaction = value; }
+    public List<TransactionModel> TransactionList { get => transactionList; set => transactionList = value; }
 }
